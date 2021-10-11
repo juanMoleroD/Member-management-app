4ee2c77c8ad344af01133d39fc3b4b96d594fa04
JuanMolero <juanldimeco@gmail.com>
2021-10-11 19:25:39 +0100

    added Springboot to the project.

---
+++ src/main/java/com/juanMoleroD/MemberManagementApp/MemberManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.juanMoleroD.MemberManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberManagementAppApplication.class, args);
+	}
+
+}
