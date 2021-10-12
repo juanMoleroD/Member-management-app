1214b970e167faab0cd7d65b8395b1c1dbe98987
JuanMolero <juanldimeco@gmail.com>
2021-10-12 20:17:28 +0100

    added Member REST controller with CRUD functionallity.

---
+++ src/main/java/com/juanMoleroD/MemberManagementApp/MemberController.java
@@ -0,0 +1,46 @@
+package com.juanMoleroD.MemberManagementApp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository repository;
+
+    @GetMapping
+    Iterable<Member> getMembers(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Member> getMemberById(@PathVariable String id){
+        return repository.findById(id);
+    }
+
+    @PostMapping
+    void postMember(@RequestBody Member member){
+        repository.save(member);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Member> updateMemberById(@PathVariable String id,
+                                            @RequestBody Member member){
+        return (repository.existsById(id)) ?
+                new ResponseEntity<>(repository.save(member), HttpStatus.OK):
+                new ResponseEntity<>(repository.save(member), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteMemberById(@PathVariable String id){
+        repository.deleteById(id);
+    }
+
+
+
+}
