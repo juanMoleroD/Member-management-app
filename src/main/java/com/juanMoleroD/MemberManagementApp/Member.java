6b6ccb6a5b080316f14bf410c7e16e736de3825c
JuanMolero <juanldimeco@gmail.com>
2021-10-16 15:47:11 +0100

    reduced complexity on entity class

--- src/main/java/com/juanMoleroD/MemberManagementApp/Member.java
+++ src/main/java/com/juanMoleroD/MemberManagementApp/Member.java
@@ -4,17 +4,14 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 @Data
 public class Member {
     @Id
     private final Long memberId;
-    private String name, surname, age, mobile;
-
-    private String addressLineOne, addressLine2, city, postcode;
+    private String name, surname, mobile;
 
     private boolean isPremium;
-
-    private String paymentPlan;
 }
