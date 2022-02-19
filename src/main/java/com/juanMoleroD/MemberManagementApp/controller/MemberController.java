c255f675e6fd04d3dd7b116221bd2b42e972f32c
juanMoleroD <juanldimeco@gmail.com>
2022-02-19 15:33:38 +0000

    Added dynamic frontend with add functionality using thymeleaf, updated the controller to generate the HTML templates and added styling to the table.

--- src/main/java/com/juanMoleroD/MemberManagementApp/controller/MemberController.java
+++ src/main/java/com/juanMoleroD/MemberManagementApp/controller/MemberController.java
@@ -5,19 +5,31 @@ import com.juanMoleroD.MemberManagementApp.model.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
-@RestController
+@Controller
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberRepository repository;
 
+
     @GetMapping
-    Iterable<Member> getMembers(){
-        return repository.findAll();
+    public String getMembers(Model model){
+        List<Member> members = new ArrayList<>();
+        Iterable<Member> foundMembers = repository.findAll();
+        foundMembers.forEach(members::add);
+
+        model.addAttribute("members", members);
+        model.addAttribute("member", new Member());
+        return "members_page";
     }
 
     @GetMapping("/{id}")
@@ -25,9 +37,12 @@ public class MemberController {
         return repository.findById(id);
     }
 
-    @PostMapping
-    void postMember(@RequestBody Member member){
+    @PostMapping("/addMember")
+    String postMember(@ModelAttribute Member member){
+        System.out.println("member received is: " + member);
         repository.save(member);
+
+        return "redirect:/members";
     }
 
     @PutMapping("/{id}")
