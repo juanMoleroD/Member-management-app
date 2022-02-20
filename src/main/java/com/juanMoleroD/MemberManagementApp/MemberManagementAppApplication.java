8d0061bb04af426b77153d20e28b250c2c6a1e67
juanMoleroD <juanldimeco@gmail.com>
2022-02-20 13:04:48 +0000

    added Swagger

--- src/main/java/com/juanMoleroD/MemberManagementApp/MemberManagementAppApplication.java
+++ src/main/java/com/juanMoleroD/MemberManagementApp/MemberManagementAppApplication.java
@@ -1,8 +1,11 @@
 package com.juanMoleroD.MemberManagementApp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info =@Info(title = "Member Management API", version = "1.0", description = "Handles members information on the database."))
 @SpringBootApplication
 public class MemberManagementAppApplication {
 
